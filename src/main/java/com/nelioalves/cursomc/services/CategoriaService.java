1086ac2ca5a40153fb84de6ec16b7168419dba28
Edelson Junior <elcj@cin.ufpe.br>
2019-03-15 13:07:37 -0300

    Correcao 1: update em CategoriaService

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -32,9 +32,11 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	public void delete(Integer id) {
 		find(id);
@@ -56,5 +58,11 @@ public class CategoriaService {
 	}
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
+		
 	}
 }
