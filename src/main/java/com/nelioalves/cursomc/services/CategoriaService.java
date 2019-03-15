10260fbd6112ff5c7951de61f41ce935487d45b4
Edelson Junior <elcj@cin.ufpe.br>
2019-03-15 13:09:49 -0300

    Correcao 2: update em CategoriaService

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
-
+ 
 @Service
 public class CategoriaService {
 
