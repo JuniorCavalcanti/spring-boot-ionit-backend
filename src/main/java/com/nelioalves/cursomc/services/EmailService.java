434760d8092bfecea215b85ff08aa3fcda1a7e38
Edelson Junior <elcj@cin.ufpe.br>
2019-03-20 07:58:10 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,13 +1,20 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
 
+	//SEND EMAIL FROM PLAN TEXT
 	void sendOrderConfirmationEmail(Pedido obj);
-	
 	void sendEmail(SimpleMailMessage msg);
+	
+	//SEND EMAIL FROM HTML
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
+	
 	
 }
